b7295d5f3d357e6c61be1a263163ac1ddaa95108
luo.wenxing <luo.wenxing@xiaoyi.com>
2017-09-11 19:23:11 +0800

    增加配置中心

--- configClient/src/main/java/com/season/client/controller/TestController.java
+++ configClient/src/main/java/com/season/client/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.season.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class TestController {
 
-/*    @Value("${name}")
-    private String from;*/
+    @Value("${from}")
+    private String from;
 
     @Autowired
     private Environment environment;
@@ -17,7 +18,7 @@ class TestController {
 
     @RequestMapping("/from")
     public String from() {
-//        return this.from;
+        System.out.println(this.from);
         String s =  environment.getProperty("from");
         return s;
     }
