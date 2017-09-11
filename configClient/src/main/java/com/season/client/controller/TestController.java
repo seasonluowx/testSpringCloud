ca426f09d09907708b03f9f06da605cc9e98aed9
luo.wenxing <luo.wenxing@xiaoyi.com>
2017-09-11 18:53:17 +0800

    增加配置中心

--- configClient/src/main/java/com/season/client/controller/TestController.java
+++ configClient/src/main/java/com/season/client/controller/TestController.java
@@ -1,18 +1,24 @@
 package com.season.client.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
-public class TestController {
-    @Value("${testconfigenv}")
-    private String testconfigenv;
+class TestController {
+
+/*    @Value("${name}")
+    private String from;*/
+
+    @Autowired
+    private Environment environment;
+
 
     @RequestMapping("/from")
-    public String from(){
-        return this.testconfigenv;
+    public String from() {
+//        return this.from;
+        String s =  environment.getProperty("from");
+        return s;
     }
 }
